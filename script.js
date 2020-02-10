37b7f35c208aea7262a2c1a37ec65126fee14b94
konles <antonio.esgutierrez@gmail.com>
2020-02-10 14:32:55 -0600

    creating startpage

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+document.getElementById("searchTerm").addEventListener("keyup", function(event) {
+	if (event.keyCode == 13) {
+		window.open("https://www.startpage.com/do/dsearch?query=" + document.getElementById("searchTerm").value);
+		document.getElementById("searchTerm").value = "";
+	}
+});
+};
+
+function updateTime() {
+	let dt = new Date();
+	let hours = dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours();
+	let minutes = dt.getMinutes() < 10 ? "0" + dt.getMinutes() : dt.getMinutes();
+	let seconds = dt.getSeconds() < 10 ? "0" + dt.getSeconds() : dt.getSeconds();
+	document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(updateTime, 3000);
